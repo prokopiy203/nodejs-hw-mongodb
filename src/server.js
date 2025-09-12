5fb7f9037e0b30526f0f2c1f7266a6fb9a337428
Vlad Prokopiy <vladprokopiy@gmail.com>
2025-09-12 21:10:57 +0300

    add-swagger/contacts&auth_endpoints

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -22,6 +23,7 @@ export const setupServer = () => {
       },
     }),
   );
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
